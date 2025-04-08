0990019cde2883fc3c961bf2196e327dd874420e
VALDEMIR ARAUJO ANDRADE <valdemiraraujo.github@gmail.com>
2025-04-08 19:00:31 -0300

    atu

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 let circulo = document.querySelector(".circulo");
 let imagem = document.querySelector(".ima");
+let trocarcorbotao = document.querySelector(".mudar");
 
 function trocarCor(cor) {
   circulo.style.background = cor;
@@ -7,4 +8,8 @@ function trocarCor(cor) {
 
 function trocarImagem(img) {
   imagem.src = img;
+}
+
+function mudarcor(corr) {
+  trocarcorbotao.style.background = corr;
 }
